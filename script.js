2830048991bc3062fb168704d49d1753f4ef1e88
emilyrudolph <107376414+emilyrudolph@users.noreply.github.com>
2022-06-27 15:38:06 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,53 @@
+	$(".purdue").click(function() {
+        $(".class1").hide();
+        $(".class3").show();
+    });
+
+	$(".embry").click(function() {
+        $(".class1").hide();
+    	$(".class2").show();
+	});
+
+	$(".suffer").click(function() {
+        $(".class3").hide();
+        $(".class6").show();
+    });
+
+	$(".find").click(function() {
+        $(".class3").hide();
+        $(".class14").show();
+    });
+
+	$(".takeover").click(function() {
+        $(".class4").hide();
+        $(".class8").show();
+    });
+
+	$(".killu").click(function() {
+        $(".class6").hide();
+        $(".class12").show();
+    });
+
+	$(".find").click(function() {
+        $(".class6").hide();
+        $(".class14").show();
+    });
+
+$(".restart").click(function() {
+        $(".class8").hide();
+    	$(".class9").hide();
+   	 	$(".class10").hide();
+    	$(".class12").hide();
+    	$(".class7").hide();
+    $(".class2").hide();
+    $(".class4").hide();
+    $(".class5").hide();
+    $(".class3").hide();
+    $(".class6").hide();
+    $(".class14").hide();
+        $(".class1").show();
+    
+    });
+
+
+
